43137b2f2b6fd86be706413ff9425b44a6a7ec1c
Aryeh Harris <harryttd@gmail.com>
2017-04-22 22:54:50 -0400

    Made blacklist remove first occurence of angle brackets regex much better.

--- server/server.js
+++ server/server.js
@@ -15,17 +15,17 @@ app
     bodyParser.urlencoded({ extended: true }), bodyParser.json(),
     express.static(resolve(__dirname, '..', 'public'))
   )
-  // .get('/', (req, res, next) => res.sendFile(resolve(__dirname, '..', 'public', 'index.html')))
   .get('*', (req, res, next) => res.sendFile(resolve(__dirname, '..', 'public', 'index.html')))
   .post('/', (req, res, next) => {
     const { list, encode, selectedOptions } = req.body;
     let { input } = req.body;
     const validators = Object.values(blacklist).filter(func => selectedOptions.includes(func.title));
 
+    // DO WE ENCODE FIRST OR RUN VALIDATORS FIRST??
+    if (encode) input = entityEncoder.htmlEncode(input);
     validators.forEach(func => input = func(input));
 
-    if (encode) res.send(entityEncoder.htmlEncode(input));
-    else res.send(input);
+    res.send(input);
   });
 
 app.listen(3000, () => console.log(chalk.green('The server is listening on port 3000!')));
--- utils/blacklist.js
+++ utils/blacklist.js
@@ -2,12 +2,13 @@
 
 const blacklist = {
   // Remove first occurence of script tag angle brackets
-  firstAngleBrackets: (input) => input.replace(/<(script)>(.*)(?:<\/)(script)>/g, (_, a, b, c) =>  c ? a + b + c : a + b),
+  // /<(script)>([^<]*)(?:<\/(script)>)?/g
+  firstAngleBrackets: (input) => input.replace(/<\/?(script)>/g, (_, a) => a),
   // Remove script tags entirely (lowercase sensitive)
   removeScriptTags: (input) => input.replace(/<+script>+([^<]*)(?:<*\/*script>*)?/g, (_, a) =>  a)
 };
 
-blacklist.firstAngleBrackets.title = "Remove first occurence angle brackets of script tag";
-blacklist.removeScriptTags.title = "Remove script tags";
+blacklist.firstAngleBrackets.title = "Remove first occurence of script tag angle brackets";
+blacklist.removeScriptTags.title = "Remove script tags completely";
 
 module.exports = { blacklist };
